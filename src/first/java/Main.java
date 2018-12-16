6055a1bc7bf16a4c1f5995923f49f9ed79cad260
Beatrice <beatrice.bucur@gmail.com>
2018-12-17 00:32:31 +0200

    first project commit

---
+++ src/first/java/Main.java
@@ -0,0 +1,29 @@
+package first.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean mosquito_alive;
+        boolean eats_salt;
+        boolean drinks_Tequila;
+        boolean trips_on_stick;
+        boolean fatal_head_injury;
+        while (mosquito_alive = true) {
+            if (eats_salt = true) {
+                System.out.println("Get thirsty");
+            }
+            if (drinks_Tequila = true) {
+                System.out.println("Get drunk");
+            }
+            if (trips_on_stick = true) {
+                System.out.println("Hits head over rock");
+            }
+            if (fatal_head_injury = true) {
+                System.out.println("Fatal head injury");
+                mosquito_alive = false;
+            }
+            System.out.println("Mosquito R.I.P-->Mission accomplished");
+            break;
+        }
+    }
+}
